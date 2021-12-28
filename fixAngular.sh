ae4f612c65934f1a531929059733a31c0ced0bdd
Shachar297 <thisishachar@gmail.com>
2021-12-28 11:23:00 +0200

    commit

--- fixAngular.sh
+++ fixAngular.sh
@@ -60,22 +60,25 @@ cd app/modules
 rm app.module.ts
 
 echo "
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { LayoutComponent } from './components/layout/layout.component';
+
 import { HeaderComponent } from '../components/header/header.component';
 import { MenuComponent } from '../components/menu/menu.component';
 import { LoginComponent } from '../components/login/login.component';
 import { FooterComponent } from '../components/footer/footer.component';
+import { LayoutComponent } from '../components/layout/layout.component';
 
 @NgModule({
   declarations: [
     HeaderComponent,
     MenuComponent,
     LoginComponent,
-    FooterComponent
+    FooterComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
