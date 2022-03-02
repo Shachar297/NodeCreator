2ac3e428acaa82bc2957bea567196906173d3bc3
Shachar297 <thisishachar@gmail.com>
2022-03-02 12:58:57 +0200

    commit

--- deliver.sh
+++ deliver.sh
@@ -4,10 +4,15 @@ FULL_PATH=~/Desktop/NodeMaker
 
 cd ${FULL_PATH}
 
-cp -r . ${1}
+cp -r *.sh ${1}
 
 cd $1
 
-rm -rf .git
-rm .DS_Store
-rm -rf images
+# rm -rf .git
+# rm .DS_Store
+# rm -rf images
+# rm -rf .history
+# rm README.md
+# rm .gitignore
+
+chmod +x *.sh
